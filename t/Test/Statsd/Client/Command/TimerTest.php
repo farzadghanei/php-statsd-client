473c685b58641b57cfe5e35b97e86a99ad73d502
Farzad Ghanei <farzad.ghanei@gmail.com>
2016-10-11 13:57:59 +0200

    Add timeCallable() to Timer
    So we have an explicit method to time a callalbe and send timing metrics
    for the duration.
    This is to deprecate passing different types of arguments to timing().

--- t/Test/Statsd/Client/Command/TimerTest.php
+++ t/Test/Statsd/Client/Command/TimerTest.php
@@ -10,7 +10,7 @@ class TimerTest extends \PHPUnit_Framework_TestCase
     {
         $timer = new Timer();
         $this->assertEquals(
-            array('timing', 'timingSince'),
+            array('timing', 'timingSince', 'timeCallable'),
             $timer->getCommands()
         );
     }
@@ -63,7 +63,6 @@ class TimerTest extends \PHPUnit_Framework_TestCase
      */
     public function testTimeCallable($callable)
     {
-        $this->markTestIncompelete('not implemented yet');
         $timer = new Timer();
         $result = $timer->timeCallable('foo.bar', $callable);
         $this->assertRegExp('/foo.bar:\d+|ms/', $result);
@@ -71,8 +70,57 @@ class TimerTest extends \PHPUnit_Framework_TestCase
 
     public function provideCallableValues()
     {
+        $simpleClosure = function () { usleep(1); };
+
         return array(
-            'function name string' => array('phpinfo')
+            'function name string' => array('time'),
+            'closure' => array($simpleClosure),
+            'object method array' => array(array($this, 'sleep1MicroSecond')),
+            'class method array' => array(array('\Test\Statsd\Client\Command\TimerTest', 'staticSleep1MicroSecond')),
+        );
+    }
+
+    /**
+     * This is to be called as an object method callable by
+     * data provider for testTimeCallable.
+     */
+    public function sleep1MicroSecond()
+    {
+        usleep(1);
+    }
+
+    /**
+     * This is to be called as a static method callable by
+     * data provider for testTimeCallable.
+     */
+    public static function staticSleep1MicroSecond()
+    {
+        usleep(1);
+    }
+
+    /**
+     * @dataProvider provideNoneCallableValues
+     */
+    public function testTimeCallableThrowsExceptionOnNoneCallableParams($noneCallable)
+    {
+        $timer = new Timer();
+        $this->setExpectedException('\InvalidArgumentException');
+        $timer->timeCallable('foo.bar', $noneCallable);
+    }
+
+    public static function provideNoneCallableValues()
+    {
+        return array(
+            'integer' => array(100),
+            'float' => array(302.455),
+            'bool flase' => array(false),
+            'bool true' => array(true),
+            'string' => array('no such function exists'),
+            'array' => array(array(1,2)),
+            'object' => array($this),
+            'class' => array('\PHPUnit_Framework_TestCase'),
+            'object in array' => array(array($this)),
+            'class in array' => array('\Test\Statsd\Client\Command\TimerTest')
         );
     }
 }
